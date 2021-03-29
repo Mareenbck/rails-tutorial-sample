283db297d67d466145031097b9e30ba691efd9df
Marine Bascunana <marine.bascunana@gmail.com>
2021-03-29 11:10:54 +0200

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['app205555281@heroku.com'],
+    :password => ENV['qzu6pvxo7727'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
